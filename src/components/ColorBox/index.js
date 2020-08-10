ab19314f7558da5e0f28a07bcdc7597eb76589a0
VoTan <vdt040499@gmail.com>
2020-08-10 11:43:01 +0700

    ColorBox Practice

---
+++ src/components/ColorBox/index.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './ColorBox.scss';
+function getRandomColor() {
+    const COLOR_LIST = ['deeppink','green','yellow','black','blue']
+    const randomIndex = Math.trunc(Math.random() * 5);
+
+    return COLOR_LIST[randomIndex];
+}
+
+function ColorBox() {
+    const [color, setColor] = useState(() => {
+        const initColor = localStorage.getItem('color') || 'deeppink';
+        return initColor;
+    });
+
+    function handleBoxClick() {
+        //get random color -> set color
+        const newColor = getRandomColor();
+        localStorage.setItem('color', newColor);
+        setColor(newColor);
+    }
+
+    return (
+        <div 
+            className="color-box" 
+            style={{ backgroundColor: color }}
+            onClick={handleBoxClick}
+        >
+        </div>
+    );
+}
+
+export default ColorBox;
