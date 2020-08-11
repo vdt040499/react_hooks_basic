f4b8f69a82622bdf2bec52a0acbc6860a34774f7
VoTan <vdt040499@gmail.com>
2020-08-11 12:21:35 +0700

    API with useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
-import ToDoList from './components/ToDoList/index';
-import ToDoForm from './components/ToDoForm/index';
+// import ToDoList from './components/ToDoList/index';
+// import ToDoForm from './components/ToDoForm/index';
+import PostList from './components/PostList/index';
+
 function App() {
 
   const [toDoList, setToDoList] = useState([
@@ -9,6 +11,30 @@ function App() {
     { id: 2, title: 'We love Easy Frontend! 🥰 ' },
     { id: 3, title: 'They love Easy Frontend! 🚀 ' },
   ]);
+
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    async function fetchPostList() {
+      try {
+        const requestUrl = 'http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1';
+        const response = await fetch(requestUrl);
+        const responseJSON = await response.json();
+        console.log(responseJSON);
+
+        const { data } = responseJSON;
+        setPostList(data);
+      } catch (error) {
+        console.log('Failed to fetch post list: ', error.message);
+      }
+    }
+    console.log('POSTLIST effect');
+    fetchPostList();
+  }, []);
+
+  useEffect(() => {
+    console.log('TODOLIST effect');
+  })
 
   function handleToDoClick(todo){
     const index = toDoList.findIndex(x => x.id === todo.id)
@@ -33,8 +59,9 @@ function App() {
 
   return (
     <div className="app">
-      <ToDoForm onSubmit={handleToDoFormSubmit}/>
-      <ToDoList todos={toDoList} onToDoClick={handleToDoClick}/>
+      {/* <ToDoForm onSubmit={handleToDoFormSubmit}/> */}
+      {/* <ToDoList todos={toDoList} onToDoClick={handleToDoClick}/> */}
+      <PostList posts={postList}/>
     </div>
   );
 }
